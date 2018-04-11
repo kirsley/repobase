67e8644f93f147522027e67cf2731037b4500753
kirsley <kirsley@gmail.com>
2018-04-11 09:55:10 +0200

    update tcs3 plugin

--- wp-content/plugins/tcs3/tcS3.php
+++ wp-content/plugins/tcs3/tcS3.php
@@ -1,23 +1,39 @@
 <?php
 /**
- * Plugin Name: TCS3 -- Send uploads directly to S3
- * Plugin URI: http://tcm.io
+ * Plugin Name: tcS3 -- Send uploads directly to S3
+ * Plugin URI: https://mccarthydigitalconsulting.com
  * Description: Allows site admins to push uploads to S3
- * Version: 1.9.1
- * Author: TC McCarthy
- * Author URI: http://tcm.io
+ * Version: 2.1.1
+ * Author: TC McCarthy, McCarthy Digital Consulting
+ * Author URI: https://mccarthydigitalconsulting.com
  * License: GPL2
  */
 
-$root = dirname(__FILE__) . "/";
-$version = "1.9";
+ // composer autoloader
+ require_once(__DIR__ . "/vendor/autoload.php");
 
-require($root . "aws/aws-autoloader.php");
-require_once($root . "classes/tcS3.class.php");
-require_once($root . "classes/tcS3_wp.class.php");
+ class tcs3
+ {
+     public function __construct()
+     {
+         add_action("after_setup_theme", [$this, "init"], 20);
+         add_action("admin_enqueue_scripts", [$this, "scripts"], 20);
+     }
 
-//instantiate class with base ops
-$tcS3 = (object) array(
-    "_base" => new tcS3(),
-    "_wp" => new tcS3_WP()
-);
+     public function init()
+     {
+         $this->base_ = new tcs3_base();
+         $this->aws_ops_ = new tcs3_aws_ops($this->base_->options);
+         $this->wp_media_ = new tcs3_wp_media($this->base_->options);
+         $this->wp_options_ = new tcs3_wp_options($this->base_->options);
+         $this->wp_ajax_ = new tcs3_ajax();
+     }
+
+     public function scripts()
+     {
+         wp_enqueue_script("jquery");
+         wp_enqueue_script("tcs3", plugin_dir_url(__FILE__) . "js/app.min.js");
+     }
+ }
+
+ $tcS3 = new tcs3();
